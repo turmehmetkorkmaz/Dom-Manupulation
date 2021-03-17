86ac9c07394d66c909610332d94b22bb751b90b5
mehmet <turmehmetkorkmaz@gmail.com>
2021-03-17 20:59:05 +0100

    Dom temelleri

---
+++ app.js
@@ -0,0 +1,62 @@
+
+
+
+// const element = document.querySelector("#clear-todos")
+
+
+// Element ozellikleri
+
+// console.log(element.id);
+
+// console.log(element.className);
+// console.log(element.classList[0]);
+// console.log(element.textContent);
+// console.log(element.innerHTML);
+// console.log(element.href);
+// console.log(element.style);
+
+// Style ve elent ozlliklerini degilstirme
+
+// element.className = "btn btn-primary"
+// element.style.color = "#000";
+// element.style.marginLeft = "50px"
+// element.href = "https://www.googl.com.tr";
+// // element.target = "_blank"
+
+// element.textContent = "Silin";
+// element.innerHTML = "<span style = 'color:yellow'>Silin</span"
+// console.log(element);
+
+
+// const elements = document.querySelectorAll(".list-group-item")
+// elements.forEach(function(el){
+//     el.style.color = "red";
+//     el.style.background = "#eee"
+// })
+
+
+// console.log(elements);
+
+// 2. veya 4. elementi secmek 
+
+// let element2 = document.querySelector ("li:nth-child(3)")
+// element2 = document.querySelectorAll ("li:nth-child(odd)")
+// console.log(element2);
+
+
+
+
+
+
+
+
+
+
+
+// let value;
+
+// const todoList = document.querySelector(".list-group")
+// const todo = document.querySelector(".list-group-item:nth-child(2)");
+// value = todoList
+
+// console.log(value);
